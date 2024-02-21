e5b0a67a7ebccf25d429f1c361f47b368043d7f8
Dan <danretegan@yahoo.com>
2024-02-21 20:30:00 +0200

    Update reducer.jsx
    Split reducer in tasks reducer & filters reducer and use combineReducers.

--- my-app/src/redux/reducer.jsx
+++ my-app/src/redux/reducer.jsx
@@ -1,45 +1,47 @@
 import { statusFilters } from "./constants";
+// Importăm funcția de compoziție pentru reducers:
+import { combineReducers } from "redux";
 
+const taskInitialState = [
+  { id: 0, text: "Learn HTML and CSS", completed: true },
+  { id: 1, text: "Get good at JavaScript", completed: true },
+  { id: 2, text: "Master React", completed: false },
+  { id: 3, text: "Discover Redux", completed: false },
+  { id: 4, text: "Build amazing apps", completed: false },
+];
 
-const initialState = {
-  tasks: [
-    { id: 0, text: "Learn HTML and CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ],
-  filters: {
-    status: statusFilters.all,
-  },
+// Este responsabil doar pentru actualizarea proprietății tasks
+// Acum valoarea parametrului state va fi o matrice de sarcini
+const tasksReducer = (state = taskInitialState, action) => {
+  switch (action.type) {
+    case "tasks/addTask":
+      return [...state, action.payload];
+
+    case "tasks/deleteTask":
+      return state.filter((task) => task.id !== action.payload);
+
+    case "tasks/toggleCompleted":
+      return state.map((task) => {
+        if (task.id !== action.payload) {
+          return task;
+        }
+
+        return { ...task, completed: !task.completed };
+      });
+
+    default:
+      return state;
+  }
 };
 
-export const rootReducer = (state = initialState, action) => {
+const filtersInitialState = {
+  status: statusFilters.all,
+};
+
+// Este responsabil doar pentru actualizarea proprietății filters
+// Acum valoarea parametrului state va fi un obiect cu valorile filtrului
+const filtersReducer = (state = filtersInitialState, action) => {
   switch (action.type) {
-    case "tasks/addTask": {
-      return {
-        ...state,
-        tasks: [...state.tasks, action.payload],
-      };
-    }
-    case "tasks/deleteTask":
-      return {
-        ...state,
-        task: state.tasks.filter((task) => task.id !== action.payload),
-      };
-    case "tasks/toggleCompleted":
-      return {
-        ...state,
-        tasks: state.tasks.map((task) => {
-          if (task.id !== action.payload) {
-            return task;
-          }
-          return {
-            ...task,
-            completed: !task.completed,
-          };
-        }),
-      };
     case "filters/setStatusFilter":
       return {
         ...state,
@@ -53,3 +55,8 @@ export const rootReducer = (state = initialState, action) => {
   }
 };
 
+// Codul funcțiilor tasksReducer și filtersReducer:
+export const rootReducer = combineReducers ({
+    tasks: tasksReducer,
+    filters: filtersReducer,
+})
