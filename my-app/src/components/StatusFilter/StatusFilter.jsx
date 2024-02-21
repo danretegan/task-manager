9b064984f4bf4b0c32779164d3a998801c2c9ae5
Dan <danretegan@yahoo.com>
2024-02-21 11:08:25 +0200

    Update StatusFilter.jsx
    Store subscription /useSelector()

--- my-app/src/components/StatusFilter/StatusFilter.jsx
+++ my-app/src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,19 @@
 import { Button } from "../Button/Button";
 import css from "./StatusFilter.module.css";
+// Importăm hook-ul:
+import { useSelector } from "react-redux";
+// Importăm obiectul cu valorile filtrelor:
+import { statusFilters } from "../../redux/constants";
 
 export const StatusFilter = () => {
+  // Obținem valoarea filtrului din starea Redux:
+const filter = useSelector( state => state.filters.status );
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
