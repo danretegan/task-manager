d5c1c79d699e58c380f09fb676c19ecb32c00db6
Dan <danretegan@yahoo.com>
2024-02-21 16:48:20 +0200

    Update Task.jsx
    add toggleCompleted action /useDispatch()

--- my-app/src/components/Task/Task.jsx
+++ my-app/src/components/Task/Task.jsx
@@ -4,8 +4,7 @@ import PropTypes from "prop-types";
 // Importăm hook-ul:
 import { useDispatch } from "react-redux";
 // Importăm generatorul de acțiuni:
-import {deleteTask} from '../../redux/actions';
-
+import { deleteTask, toggleCompleted } from "../../redux/actions";
 
 export const Task = ({ task }) => {
   // Obținem o referință către funcția pentru trimiterea acțiunii:
@@ -13,20 +12,22 @@ export const Task = ({ task }) => {
 
   // Apelăm generatorul de acțiuni și-i transmitem id-ul sarcinii
   // Trimitem rezultatul - o acțiunea de ștergere a sarcinii:
-  const handleDelete = () => dispatch(deleteTask(task.id))
+  const handleDelete = () => dispatch(deleteTask(task.id));
+
+  // Apelăm generatorul de acțiuni și-i transmitem id-ul sarcinii
+  // Trimitem rezultatul - o acțiune de modificare a stării sarcinii:
+  const handleToggle = () => dispatch(toggleCompleted(task.id));
 
   return (
     <div className={css.wrapper}>
       <input
         type="checkbox"
+        onChange={handleToggle}
         className={css.checkbox}
         checked={task.completed}
       />
       <p className={css.text}>{task.text}</p>
-      <button 
-        type='button' 
-        onClick={handleDelete} 
-        className={css.btn}>
+      <button type="button" onClick={handleDelete} className={css.btn}>
         <MdClose size={24} />
       </button>
     </div>
