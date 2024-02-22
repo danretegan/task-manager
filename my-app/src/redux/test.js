3bc85a7b88e5480c31f518da8d2c477f824b2d66
Dan <danretegan@yahoo.com>
2024-02-22 18:28:24 +0200

    Delete reducer.js

--- my-app/src/redux/test.js
+++ my-app/src/redux/test.js
@@ -1,22 +1,49 @@
-const appState = {
-  tasks: [],
-  filters: {},
-};
+// reducer refactoring using Redux Toolkit:
+import {
+  addTask,
+  deleteTask,
+  setStatusFilter,
+  toggleCompleted,
+} from "./actions";
+import { statusFilters } from "./constants";
+import { createReducer } from "@reduxjs/toolkit";
 
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+const tasksInitialState = [
+  { id: 0, text: "Learn HTML and CSS", completed: true },
+  { id: 1, text: "Get good at JavaScript", completed: true },
+  { id: 2, text: "Master React", completed: false },
+  { id: 3, text: "Discover Redux", completed: false },
+  { id: 4, text: "Build amazing apps", completed: false },
+];
 
-const tasksInitialState = [];
-
-const tasksSlice = createSlice({
-  name: "tasks",
-  initialState: tasksInitialState,
-  reducers: {
-    addTask(state, action) {},
-    deleteTask(state, action) {},
-    toggleCompleted(state, action) {},
-  },
+export const tasksReducer = createReducer(tasksInitialState, (builder) => {
+  builder
+    .addCase(addTask, (state, action) => {
+      // ✅ Immer va înlocui acest lucru cu o operație de actualizare
+      state.push(action.payload);
+    })
+    .addCase(deleteTask, (state, action) => {
+      // ✅ Good
+      return state.filter((task) => task.id !== action.payload);
+    })
+    .addCase(toggleCompleted, (state, action) => {
+      // ✅ Immer va înlocui acest lucru cu o operație de actualizare
+      for (const task of state) {
+        if (task.id === action.payload) {
+          task.completed = !task.completed;
+          break;
+        }
+      }
+    });
 });
 
-const { addTask, deleteTask, toggleCompleted } = tasksSlice.actions;
+const filtersInitialState = {
+  status: statusFilters.all,
+};
 
-const tasksReducer = tasksSlice.reducer;
+export const filtersReducer = createReducer(filtersInitialState, (builder) => {
+  builder.addCase(setStatusFilter, (state, action) => {
+    // ✅ Immer va înlocui acest lucru cu o operație de actualizare
+    state.status = action.payload;
+  });
+});
