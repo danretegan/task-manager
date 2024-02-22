28affb1f00ca860cc5aa3cb2911e859a6f636074
Dan <danretegan@yahoo.com>
2024-02-22 12:01:32 +0200

    store refactoring using Redux Toolkit

--- my-app/src/redux/store.js
+++ my-app/src/redux/store.js
@@ -1,8 +1,6 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
+import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducer";
 
-// Extindem store-ul pentru a adăuga devTools:
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+  reducer: rootReducer,
+});
